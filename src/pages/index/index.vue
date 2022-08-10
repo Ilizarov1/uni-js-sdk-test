af2eb66bcdaf2d2c4298903cd87b1a724f1f9d03
Ilizarov1 <1224722105@qq.com>
2022-08-10 16:48:03 +0800

    update bucketName

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
+		<input type="text" v-model="bucketName" placeholder="请先输入空间名" />
 		<input type="text" v-model="secretKey" placeholder="请先输入secretKey" />
 		<input type="text" v-model="accessKey" placeholder="请先输入accessKey" />
 		<button size="mini" :disabled="false" hover-start-time=20 @click="handleClick">选择文件</button>
@@ -9,7 +10,7 @@
 <script>
 	import TokenTools from '../../utlis/token'
 	import * as qiniu from 'qiniu-js'
-	function handleUpload(file, accessKey, secretKey) {
+	function handleUpload(file, accessKey, secretKey, scope) {
 				const observer = {
 					next(res){},
 					error(res){
@@ -20,19 +21,20 @@
 					}
 				}
 				const token = TokenTools.genUpToken(accessKey, secretKey,{
-					scope: 'append-test',
+					scope,
 					deadline: Math.floor(Date.now() / 1000) + 3600
 					})
 				const observable = qiniu.upload(file, null, token)
 				const subscription = observable.subscribe(observer)
-				
+
 			}
 	export default {
 		data() {
 			return {
 				title: 'Hello',
 				accessKey: '',
-				secretKey: ''
+				secretKey: '',
+				bucketName: ''
 			}
 		},
 		onLoad() {
@@ -42,13 +44,14 @@
 			handleClick() {
 				const ak = this.accessKey
 				const sk = this.secretKey
+				const bucketName = this.bucketName
 				// 选择图片文件
 				uni.chooseFile({
 					count: 1,
 					type: 'image',
 					success (res) {
 						const tempFilePaths = res.tempFiles
-						handleUpload(tempFilePaths[0], ak, sk)
+						handleUpload(tempFilePaths[0], ak, sk, bucketName)
 					}
 				})
 			}
